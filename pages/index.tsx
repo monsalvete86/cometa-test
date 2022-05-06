1cd4f90d55ca78eaf0836c8ff89a2ab47c253346
Camilo Monsalve <camilo.monsalve@encora.com>
2022-05-05 21:21:12 -0500

    stable commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
 
   async function getStudent() {
     const response =  await fetch(
-      "http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/", 
+      "https://cors-everywhere.herokuapp.com/http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/", 
       {
         headers: {
           hash: "OcJn4jYChW",
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
 
   async function getOrders() {
     const response =  await fetch(
-      "http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders/", 
+      "https://cors-everywhere.herokuapp.com/http://ec2-3-239-221-74.compute-1.amazonaws.com:8000/api/v1/students/3b35fb50-3d5e-41b3-96d6-c5566141fab0/orders/", 
       {
         headers: {
           hash: "OcJn4jYChW",
